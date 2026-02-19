041b6f559b3561e7a26212fd6c0eac811865995e
Evelina <evelina20082707@gmail.com>
2026-02-20 00:06:45 +0200

    4 task

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,5 @@
 const formLogin = document.querySelector('.login-form');
-const message = document.createElement('p'); 
-message.style.color = 'red';
-formLogin.appendChild(message);
+
 formLogin.addEventListener('submit', e => {
   e.preventDefault();
 
@@ -11,17 +9,11 @@ const email = elements.email.value.trim();
   const password = elements.password.value.trim();
 
   if (!email || !password) {
-    message.textContent = 'All form fields must be filled in';
+    alert('All form fields must be filled in');
     return;
   }
-  message.textContent = '';
 
-   const formData = {
-    email: email,
-    password: password
-  };
-
-  console.log(formData);
+  console.log({email, password});
   formLogin.reset();
 
 })
