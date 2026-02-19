318d43cae9b847d8f790eaf8d605ce39f39409d4
Evelina <evelina20082707@gmail.com>
2026-02-19 23:51:03 +0200

    task 3 5

--- src/js/task-5.js
+++ src/js/task-5.js
@@ -1,5 +1,22 @@
+
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  
 }
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+  const newColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+
+
+
+})
